a9435deb28f165a207ff18a9ed4b82d3456c4371
saumyasingh107 <7226shivam1007@gmail.com>
2024-09-07 06:39:44 +0530

    frontend setup

---
+++ frontend/src/components/Quotes.tsx
@@ -0,0 +1,26 @@
+
+const Quotes = () => {
+  return (
+    <div className="h-screen bg-gray-300 flex justify-center flex-col">
+        <div className="flex justify-center">
+
+        <div className="max-w-lg">
+            <div className="text-3xl font-bold ">
+
+            "The customer service I received was
+exceptional. The support team went above
+and beyond to address my concerns."
+            </div>
+            <div className="max-w-md font-semibold mt-3 text-xl">
+                Jules Winfield
+            </div>
+            <div className="max-w-md text-slate-500 font-normal">
+                CEO | Acme Inc
+            </div>  
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Quotes
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,16 @@
+import Quotes from "../components/Quotes"
+
+const Signup = () => {
+  return (
+    <div className="grid grid-cols-2">
+        <div>
+            signin
+        </div>
+        <div className="invisible lg:visible">
+        <Quotes/>
+        </div>
+    </div>
+  )
+}
+
+export default Signup
