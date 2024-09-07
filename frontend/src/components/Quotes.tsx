c94c8e6082c100ada95d69f5baf7fd086f9fec3c
saumyasingh107 <7226shivam1007@gmail.com>
2024-09-08 04:59:59 +0530

    component

--- frontend/src/components/Quotes.tsx
+++ frontend/src/components/Quotes.tsx
@@ -7,10 +7,10 @@ const Quotes = ({type}:{type:"signin"|"signup"}) => {
             {type==="signin"?`"Welcome back to where your thoughts find a voice. Let's get you back to writing your story."`:`"Join us today and start sharing your ideas with the world. Every great story begins with a single step"`}
           </div>
           <div className="max-w-md font-semibold mt-3 text-xl">
-            Jules Winfield
+            {type==="signin"?"Jules Winfield":"Daniel Harper"}
           </div>
           <div className="max-w-md text-slate-500 font-normal">
-            CEO | Acme Inc
+             {type==="signin"?"CEO | Acme Inc":"Founder | WordWeave Corp"}
           </div>
         </div>
       </div>
